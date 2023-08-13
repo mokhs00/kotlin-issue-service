fcefd08bdc89beb09fdcb463609d5295a52b7ba5
mokhs00 <mokhs00@naver.com>
2023-08-14 05:51:03 +0900

    Implement JWTUtils

---
+++ user-service/src/main/kotlin/com/example/userservice/config/JWTProperties.kt
@@ -0,0 +1,11 @@
+package com.example.userservice.config
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+
+@ConfigurationProperties(prefix = "jwt")
+data class JWTProperties(
+    val issuer: String,
+    val subject: String,
+    val expiresTime: Long,
+    val secret: String,
+)
---
+++ user-service/src/test/kotlin/com/example/userservice/utils/JWTUtilsTest.kt
@@ -0,0 +1,58 @@
+package com.example.userservice.utils
+
+import com.example.userservice.config.JWTProperties
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class JWTUtilsTest {
+
+    @Test
+    fun createToken() {
+        val jwtClaim = JwtClaim(
+            userId = 1,
+            email = "mokhs00@naver.com",
+            profileUrl = "profile.jpg",
+            username = "mohks00",
+        )
+
+        val properties = JWTProperties(
+            issuer = "mokhs",
+            subject = "auth",
+            expiresTime = 3600,
+            secret = "temp-secret",
+        )
+
+        val token = JWTUtils.createToken(claim = jwtClaim, properties = properties)
+
+        assertNotNull(token)
+    }
+
+    @Test
+    fun decode() {
+        val jwtClaim = JwtClaim(
+            userId = 1,
+            email = "mokhs00@naver.com",
+            profileUrl = "profile.jpg",
+            username = "mohks00",
+        )
+
+        val properties = JWTProperties(
+            issuer = "mokhs",
+            subject = "auth",
+            expiresTime = 3600,
+            secret = "temp-secret",
+        )
+
+        val token = JWTUtils.createToken(claim = jwtClaim, properties = properties)
+
+        val decode = JWTUtils.decode(token = token, secret = properties.secret, issuer = properties.issuer)
+
+        with(decode) {
+            assertEquals(claims["userId"]!!.asLong(), jwtClaim.userId)
+            assertEquals(claims["email"]!!.asString(), jwtClaim.email)
+            assertEquals(claims["profileUrl"]!!.asString(), jwtClaim.profileUrl)
+            assertEquals(claims["username"]!!.asString(), jwtClaim.username)
+        }
+    }
+}
